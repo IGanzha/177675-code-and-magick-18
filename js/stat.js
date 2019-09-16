2d9045935a9ae4bad95ab605b9e20352b49e26d2
Ilya Ganzha <ilya.ganzha@gmail.com>
2019-09-16 23:52:04 +0300

    Добавлено окно со статистикой игроков_правки тревиса

--- js/stat.js
+++ js/stat.js
@@ -19,10 +19,10 @@ var renderCloud = function (ctx, x, y, color) {
 };
 
 window.renderStatistics = function (ctx, names, times) {
-  renderCloud(ctx, CLOUD_X + SHADOW_GAP, CLOUD_Y + SHADOW_GAP, 'rgba(0, 0, 0, 0.7)')
-  renderCloud(ctx, CLOUD_X, CLOUD_Y, 'white')
+  renderCloud(ctx, CLOUD_X + SHADOW_GAP, CLOUD_Y + SHADOW_GAP, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, 'white');
 
-  ctx.fillStyle = 'black'
+  ctx.fillStyle = 'black';
   ctx.font = '16px PT Mono';
   ctx.fillText('Ура вы победили!', CLOUD_X + PADDING_LEFT, CLOUD_Y + PADDING_VERT);
   ctx.fillText('Список результатов:', CLOUD_X + PADDING_LEFT, CLOUD_Y + PADDING_VERT + LINES_HEIGHT);
@@ -31,20 +31,20 @@ window.renderStatistics = function (ctx, names, times) {
   for (var i = 1; i < times.length; i++) {
     if (times[i] > maxResult) {
       maxResult = times[i];
-    };
-  };
+    }
+  }
 
-  for (var i = 0; i < names.length; i++) {
-    if (names[i]==='Вы') {
-      ctx.fillStyle='rgba(255, 0, 0, 1)';
+  for (i = 0; i < names.length; i++) {
+    if (names[i] === 'Вы') {
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     } else {
-      ctx.fillStyle='rgba(0, 0, 255,' + Math.random() + ')';
+      ctx.fillStyle = 'rgba(0, 0, 255,' + Math.random() + ')';
     }
 
     var playerResult = Math.round(times[i]);
     ctx.fillRect(CLOUD_X + PADDING_LEFT + (COLUMN_WIDTH + HORIZONTAL_GAP) * i, CLOUD_Y + CLOUD_HEIGHT - PADDING_VERT - LINES_HEIGHT - VERTICAL_GAP, COLUMN_WIDTH, (-1) * STAT_HEIGHT * times[i] / maxResult);
-    ctx.fillStyle='black';
+    ctx.fillStyle = 'black';
     ctx.fillText(names[i], CLOUD_X + PADDING_LEFT + (COLUMN_WIDTH + HORIZONTAL_GAP) * i, CLOUD_Y + CLOUD_HEIGHT - PADDING_VERT);
     ctx.fillText(playerResult, CLOUD_X + PADDING_LEFT + (COLUMN_WIDTH + HORIZONTAL_GAP) * i, CLOUD_Y + CLOUD_HEIGHT - PADDING_VERT - LINES_HEIGHT - (2 * VERTICAL_GAP) - STAT_HEIGHT * times[i] / maxResult);
   }
-}
+};
